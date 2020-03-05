bd1df1c943ae2a3d7c1d3114a0a90653070eef44
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-03-05 17:16:52 +0000

    Add files via upload

---
+++ Regular Expressions/match-numbers-and-letters-of-the-alphabet.js
@@ -0,0 +1,3 @@
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // i = ignore case (case insensitive) g = global (match all not just first)
+let result = quoteSample.match(myRegex); // Change this line
